88b4bb39a383f86d4f2295e3542be57a5e9a9b94
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-10 09:59:49 -0500

    update App.js with container

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;

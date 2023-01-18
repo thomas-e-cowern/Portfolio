48ee5471b59af991a5bcdb86ef02527d9e5462e5
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-18 15:06:29 -0500

    update Header.jsx to fix image issue

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => {
         className="app__header-circles"
       >
         {[images.flutter, images.redux, images.sass].map((circle, index) => (
-          <div className="cicrle-cmp app__flex" key={`circle-${index}`}>
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
         ))}
